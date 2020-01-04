8754bd1435bce96087b255b1facbdfc13869b225
Karel Madera <KarelMad@email.cz>
2020-01-04 19:15:30 +0100

    Added bunch of things.
    Added window size restriction.
    Added playing field scrolling.
    Added wind generation.
    Added tank/shell collision detection.
    
    Added possible offset to the ValueItem
    
    Changed muzzle position to prevent shell collision with the firing tank.

--- scorcher_earth.py
+++ scorcher_earth.py
@@ -6,23 +6,130 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.image import Image
 from kivy.uix.label import Label
 from kivy.uix.relativelayout import RelativeLayout
+from kivy.uix.textinput import TextInput
 from kivy.uix.widget import Widget
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.properties import NumericProperty, ReferenceListProperty, ObjectProperty, StringProperty, AliasProperty, \
     BooleanProperty
 from kivy.vector import Vector
 from kivy.uix.actionbar import ActionItem
+from kivy.core.window import Window
 from collections import deque
+from random import random
 
-MAX_SHELL_VELOCITY = 1000
+MAX_SHELL_VELOCITY = 750
 GRAVITY = 200
-DRAG_COEFFICIENT = 0.00002
+#DRAG_COEFFICIENT = 0.00002
+DRAG_COEFFICIENT = 0.00004
+INIT_MAX_WIND = 1
 INIT_ANGLE = 90
 INIT_POWER = 50
+
+Window.minimum_width = 800
+Window.minimum_height = 600
 
 
 def clamp(value, min_val, max_val):
     return max(min(value, max_val), min_val)
+
+def get_wind_text(wind):
+    if wind > 0:
+        return f"{wind:3.2f} >"
+    elif wind < 0:
+        return f"< {-wind:3.2f}"
+    else:
+        return 'NO WIND'
+
+class Rectangle:
+    """Rectangle(center, size, bl_offset)
+
+    Rectangle with it's center positioned at ``center``,
+    of the size ``size`` and with bottom left (in the axis aligned position) offset by
+    ``bl_offset`` from center, rotated by ``rotation`` degrees.
+    Center also serves as the center of rotation.
+
+    Args:
+        center (Vector): Center of the rectangle, serves as the center of rotation and defines the position of the rectangle.
+        size (Vector): Size of the rectangle in the form of (x,y) in the axis aligned position.
+        bl_offset (Vector): Offset of the bottom left corner (minimal x,y in the axis aligned position) from the center.
+        rotation (float): Angle of rotation of the rectangle from the base position.
+    """
+    def __init__(self, center, size, bl_offset, rotation):
+        self.center = center
+        self.size = size
+        self.bl_offset = bl_offset
+        self.rotation = rotation
+
+    def get_bl(self):
+        return self.get_local_bl() + self.center
+
+    def get_br(self):
+        return self.get_local_br() + self.center
+
+    def get_tl(self):
+        return self.get_local_tl() + self.center
+
+    def get_tr(self):
+        return self.get_local_tr() + self.center
+
+    def get_local_bl(self):
+        return self.get_aligned_bl().rotate(self.rotation)
+
+    def get_local_br(self):
+        return self.get_aligned_br().rotate(self.rotation)
+
+    def get_local_tl(self):
+        return self.get_aligned_tl().rotate(self.rotation)
+
+    def get_local_tr(self):
+        return self.get_aligned_tr().rotate(self.rotation)
+
+    def get_aligned_bl(self):
+        return self.bl_offset
+
+    def get_aligned_br(self):
+        return self.bl_offset + Vector(self.size.x, 0)
+
+    def get_aligned_tl(self):
+        return self.bl_offset + Vector(0, self.size.y)
+
+    def get_aligned_tr(self):
+        return self.bl_offset + self.size
+
+    def get_vertexes(self):
+        yield self.get_bl()
+        yield self.get_br()
+        yield self.get_tr()
+        yield self.get_tl()
+
+    def collide_point(self, point):
+        p = point - self.center
+        p.rotate(-self.rotation)
+        return Vector.in_bbox(p, self.get_aligned_bl(), self.get_local_tr())
+    
+    def collide_line_segment(self, v1, v2):
+        v_s = [vertex for vertex in self.get_vertexes()]
+        for i in range(4):
+            if Vector.segment_intersection(v_s[i], v_s[(i + 1) % 4], v1, v2) is not None:
+                return True
+        return self.collide_point(v1) and self.collide_point(v2)
+
+    def collide_rectangle(self, rect):
+        # check if any sides intersect
+        v_s = [vertex for vertex in self.get_vertexes()]
+        v_r = [vertex for vertex in rect.get_vertexes()]
+
+        for i in range(4):
+            for j in range(4):
+                if Vector.segment_intersection(v_s[i], v_s[(i + 1) % 4], v_r[j], v_r[(j + 1) %4]) is not None:
+                    return True
+
+        # check if all rect vertexes are inside self
+        for i in range(4):
+            if not self.collide_point(v_r[i]):
+                return False
+        # rect is inside the rectangle
+        return True
 
 
 class ValueItem(BoxLayout, ActionItem):
@@ -31,9 +138,17 @@ class ValueItem(BoxLayout, ActionItem):
     max = NumericProperty(0)
     min = NumericProperty(0)
     reverse = BooleanProperty()
+    # difference between the displayed value and the value of the value property
+    # +90 means displayed value of 90 equals the real value of 0
+    value_offset = NumericProperty(0)
+    text_in = ObjectProperty(None)
+    slide_in = ObjectProperty(None)
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+
+    def manual_validate_text(self):
+        self.text_in.dispatch('on_text_validate')
 
 
 class TextItem(Label, ActionItem):
@@ -83,9 +198,12 @@ class Shell(Image):
 
     angle = AliasProperty(get_angle, None, bind=["velocity"])
 
+    def get_rectangle(self):
+        return Rectangle(Vector(*self.center), Vector(*self.size), Vector(-self.width/2, -self.height/2), self.get_angle())
+
 
 class GunBarrel(Image):
-    angle = NumericProperty(0)
+    angle = NumericProperty(INIT_ANGLE)
     b_length = NumericProperty(0.5)
     b_width = NumericProperty(0.2)
     b_size = ReferenceListProperty(b_length, b_width)
@@ -105,10 +223,22 @@ class Tank(RelativeLayout):
         self.barrel.color = color
         self.barrel.angle = barrel_angle
 
-    def get_muzzle_pos(self):
-        rel_pos = Vector(self.barrel.b_length * self.width, 0).rotate(self.barrel.angle)
+    def get_muzzle_pos(self, shell_length):
+        rel_pos = Vector(self.barrel.b_length * self.width + shell_length / 2, 0).rotate(self.barrel.angle)
         rel_pos += Vector(*self.center)
         return rel_pos
+
+    def collide_with(self, shell):
+        body_rect = Rectangle(Vector(*self.to_parent(self.body.center_x, self.body.center_y)),
+                              Vector(*self.body.size),
+                              -Vector(*self.body.size) / 2,
+                              0)
+        barrel_rect = Rectangle(Vector(*self.to_parent(self.barrel.x, self.barrel.center_y)),
+                                Vector(*self.barrel.size),
+                                Vector(0, -self.barrel.height / 2),
+                                self.barrel.angle)
+        shell_rect = shell.get_rectangle()
+        return shell_rect.collide_rectangle(body_rect) or shell_rect.collide_rectangle(barrel_rect)
 
 
 class Player:
@@ -127,8 +257,7 @@ class Player:
         self.tank = tank
 
     def collide_with(self, shell):
-        #TODO: Collision
-        return False
+        return self.tank.collide_with(shell)
 
 
 class Tracer:
@@ -211,18 +340,20 @@ class Game(Screen):
         super().__init__(**kwargs)
         self.tracer = None
         self.shell = None
+        self.wind = None
         self.update_event = None
         self.players = []
         self.c_player_idx = 0
         self.angle_in.bind(value=self.on_angle_input)
         self.fire_button.bind(on_press=self.on_fire)
+        self.max_wind = INIT_MAX_WIND
 
     def on_pre_enter(self, *args):
         for idx, player in enumerate(self.players):
-            tank = Tank(player.color, self.angle_in.value)
+            tank = Tank(player.color, INIT_ANGLE)
             self.map.add_widget(tank)
             player.set_tank(tank)
-            tank.pos = (idx * 100, 20)
+            tank.pos = (idx * 400, 20)
         self.c_player_idx = len(self.players) + 1
         self.switch_player()
         self.update_event = Clock.schedule_interval(self.update, self.FRAME_RATE)
@@ -232,7 +363,6 @@ class Game(Screen):
 
     def update(self, dt):
         if self.shell is not None:
-            # TODO: SWitch back to real dt
             self.shell.update(dt)
             if self.check_collisions(self.shell):
                 self.tracer.end()
@@ -246,7 +376,9 @@ class Game(Screen):
                 self.switch_player()
 
     def check_collisions(self, shell):
-        if self.collide_players(shell):
+        player = self.collide_players(shell)
+        if player is not None:
+            player.tank.body.color = (0, 0, 0, 1)
             return True
         if self.map.terrain_collision(shell):
             return True
@@ -254,45 +386,64 @@ class Game(Screen):
 
     def switch_player(self):
         self.c_player_idx = (self.c_player_idx + 1) % len(self.players)
-        self.player_out.text = self.get_c_player().name
+        self.wind = self.generate_wind()
         self.map.trace_display.clear()
+
         if len(self.get_c_player().traces) > 0:
             last_trace = self.get_c_player().traces[-1]
             self.map.trace_display.draw_trace(last_trace.points,
                                               self.map.trace_display.colors["previous"])
-            self.angle_in.value = last_trace.angle
-            self.power_in.value = last_trace.power
+            self.set_bar_display(self.get_c_player().name,
+                                 self.get_c_player().color,
+                                 last_trace.angle,
+                                 last_trace.power,
+                                 self.wind)
+
         else:
             # first switch of players
-            self.angle_in.value = INIT_ANGLE
-            self.power_in.value = INIT_POWER
-
+            self.set_bar_display(self.get_c_player().name,
+                                 self.get_c_player().color,
+                                 INIT_ANGLE,
+                                 INIT_POWER,
+                                 self.wind)
         self.enable_input()
 
+    def set_bar_display(self, player_name, player_color, angle, power, wind):
+        self.player_out.text = player_name
+        self.player_out.color = player_color
+        self.wind_out.text = get_wind_text(wind)
+        self.angle_in.value = angle
+        self.power_in.value = power
+
     def shoot(self, player):
-        self.shell = Shell(self.power_in.value, self.angle_in.value, GRAVITY, 0, DRAG_COEFFICIENT)
+        self.shell = Shell(self.power_in.value, self.angle_in.value, GRAVITY, self.wind, DRAG_COEFFICIENT)
         self.map.add_widget(self.shell, canvas='after')
         # update the size of the shell based on the map size
         self.map.do_layout()
-        self.shell.center = player.tank.get_muzzle_pos()
+        self.shell.center = player.tank.get_muzzle_pos(self.shell.width)
         # draw the shell above everything else
         self.tracer = Tracer(self.map.trace_display, self.shell)
 
     def collide_players(self, shell):
         for player in self.players:
             if player.collide_with(shell):
-                return True
-        return False
+                return player
+        return None
 
     def on_angle_input(self, instance, value):
         self.get_c_player().tank.barrel.angle = value
 
     def on_fire(self, instance):
         self.disable_input()
+        # check if player was writing angle value and forgot to hit enter
+        self.angle_in.manual_validate_text()
         self.shoot(self.get_c_player())
 
     def get_c_player(self):
         return self.players[self.c_player_idx]
+
+    def generate_wind(self):
+        return (random() - 0.5) * self.max_wind
 
     def disable_input(self):
         self.power_in.disabled = True
